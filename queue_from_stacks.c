1e6212fbd084c3d94295b761304d10d8f1359e05
Caden Johnsen <johnsenc@flip1.engr.oregonstate.edu>
2020-08-30 20:58:17 -0700

    Final Version

--- queue_from_stacks.c
+++ queue_from_stacks.c
@@ -2,9 +2,6 @@
  * This is the file in which you'll write the functions required to implement
  * a queue using two stacks.  Make sure to add your name and @oregonstate.edu
  * email address below:
- *
- * Name:
- * Email:
  */
 
 #include <stdio.h>
